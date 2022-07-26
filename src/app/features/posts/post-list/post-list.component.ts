5e6ab64dacbc07ef082fb6b2d2b544fe07346e09
Mark Lester Echavez <marklester.echavez@gmail.com>
2022-07-27 01:44:01 +0800

    add posts

--- src/app/features/posts/post-list/post-list.component.ts
+++ src/app/features/posts/post-list/post-list.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit {
+  @Input() title: string | undefined;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/features/posts/posts.module.ts
+++ src/app/features/posts/posts.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostsComponent } from './posts.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { PostCardComponent } from './post-card/post-card.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    children: [{ path: '', pathMatch: 'full', component: PostsComponent }],
+  },
+];
 @NgModule({
-  declarations: [PostListComponent, PostsComponent],
-  imports: [CommonModule],
+  declarations: [PostListComponent, PostsComponent, PostCardComponent],
+  imports: [SharedModule, RouterModule.forChild(routes)],
 })
 export class PostsModule {}
